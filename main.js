c153ce6c87ba23200244b645f73f209902ba5238
shubhamlole <shubhamlole2@gmail.com>
2024-02-26 21:00:17 +0530

    added projects and links

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ var typed = new Typed(".text", {
     backDelay: 1000,
     loop: true,
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: "smooth"
+                });
+            }
+        })
+    })
+}) 
